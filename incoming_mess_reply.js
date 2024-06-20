f3a48ab5463bfe85ea342ac2b3768e65c9f64c18
SURIYALAKSHMI M <suriyalakshmi413@gmail.com>
2024-06-20 22:37:25 +0530

    Update incoming_mess_reply.js

--- incoming_mess_reply.js
+++ incoming_mess_reply.js
@@ -4,7 +4,7 @@ exports.handler = function(context, event, callback) {
   console.log(`Incoming message: ${event.Body}`);
 
 	let twiml = new Twilio.twiml.MessagingResponse();
-  twiml.message('Thanks for your submission!');
+  twiml.message('Thanks for Sharing!');
 
   console.log(`TwiML was ${twiml}`);
   return callback(null, twiml);
