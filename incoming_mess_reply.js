ad882575f1da2ddf9d0dbab869aae9cc58fe7b7b
SURIYALAKSHMI M <suriyalakshmi413@gmail.com>
2024-06-20 22:16:56 +0530

    Create incoming_mess_reply.js

---
+++ incoming_mess_reply.js
@@ -0,0 +1,11 @@
+// A function created in twilio, in order to response the incoming messages from the user to the twilio number
+
+exports.handler = function(context, event, callback) {
+  console.log(`Incoming message: ${event.Body}`);
+
+	let twiml = new Twilio.twiml.MessagingResponse();
+  twiml.message('Thanks for your submission!');
+
+  console.log(`TwiML was ${twiml}`);
+  return callback(null, twiml);
+};
